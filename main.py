e1d2f38d9a0e9ce8448e3a055649e452e5804d3b
Junanjunan <taltalrealty@gmail.com>
2023-11-20 20:06:15 +0900

    Query: Another example of using query

--- main.py
+++ main.py
@@ -25,6 +25,11 @@ def read_item(item_id: int, q: Annotated[Union[str, None], Query(max_length=5)] 
     return {"item_id": item_id, "q": q}
 
 
+@app.get("/items2/{item_id}")
+def read_item_2(item_id: int, q: Union[str, None] = Query(default=None, max_length=5)):
+    return {"item_id": item_id, "q": q}
+
+
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
