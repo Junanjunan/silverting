f82dcd18c7fbb01995d24485fbb70cef8e9c8a04
Junanjunan <taltalrealty@gmail.com>
2023-12-07 00:55:56 +0900

    DB: Connect to database

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Depends
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.pool import QueuePool
+from sqlalchemy.exc import OperationalError
 
+from local_settings import *
+
+
+DATABASE_URL = f"{DB_ENGINE}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(
+    DATABASE_URL,
+    poolclass=QueuePool,
+    pool_size=20,
+    max_overflow=40,
+    pool_timeout=60
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, expire_on_commit=True)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app = FastAPI()
 
 @app.get("/")
-async def index():
+async def index(request: Request, db: Session = Depends(get_db)):
+    try:
+        db.execute(text("SELECT 1"))
+        print("Connected to the database successfully!")
+    except OperationalError as e:
+        print(f"Failed to connect to the database. Error: {e}")
     return {"hello": "world"}
